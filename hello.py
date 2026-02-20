333afbe6ddbe9799284beb22e14a337819c1cdf4
Konstantinos Gus Hatzopoulos <k.hatzopoulos@protonmail.com>
2026-02-20 18:46:09 +0200

    QA

--- hello.py
+++ hello.py
@@ -1,14 +1,1 @@
-# # %%
-# import sys, requests
-# print(sys.executable)
-# print(requests.__version__)
-# # %%
-
-# %%
-import requests
-
-# Download a web page
-response = requests.get("https://api.github.com")
-print(response.status_code)
-# Should print 200
-# %%
+print("Hello World!")
