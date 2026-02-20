b8b624ca1a92600bda0b419dd0a16ec4c40565bf
Konstantinos Gus Hatzopoulos <k.hatzopoulos@protonmail.com>
2026-02-21 00:38:12 +0200

    QA

--- hello.py
+++ hello.py
@@ -1,6 +1,4 @@
 age = 25
-has_licence = True
-drunk = True
+has_licence = False
 
-can_drive = age >= 16 and has_licence and not drunk
-print = f"That man {can_drive}"
+my_list = ["Alice", 25, age, True]
