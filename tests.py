50387b6d1709296d68a34d708ee07f084510f7a0
Konstantinos Gus Hatzopoulos <k.hatzopoulos@protonmail.com>
2026-02-21 01:22:43 +0200

    QA

--- tests.py
+++ tests.py
@@ -2,3 +2,11 @@ age = 25
 has_licence = False
 
 my_list = ["Alice", 25, age, True]
+
+my_list[0] = "Dave"
+
+my_list.append("Alice")
+my_list.remove("Alice")
+my_list.insert(0, "Alice")
+
+print(my_list)
