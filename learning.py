416df060493502805a6e046fbb90f9402c11ce48
Konstantinos Gus Hatzopoulos <k.hatzopoulos@protonmail.com>
2026-02-21 16:32:19 +0200

    Learning simple

--- learning.py
+++ learning.py
@@ -1,14 +1,1 @@
-# # Date and time
-# import datetime
-# today = datetime.date.today()
-# print(today)  # 2024-01-15
-
-# Operating system
-import os
-current_dir = os.getcwd()
-print(current_dir)
-
-# # JSON data
-# import json
-# data = {"name": "Alice", "age": 30}
-# json_string = json.dumps(data)
+import pandas as pd
