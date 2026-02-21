4fb2df7bea53a433665e776499453e45e61f1d66
Konstantinos Gus Hatzopoulos <k.hatzopoulos@protonmail.com>
2026-02-21 15:35:29 +0200

    Simple learning

--- learning.py
+++ learning.py
@@ -1,11 +1,7 @@
-def add_print(a, b):
-    print(a + b)
-
-add_print(a=5, b=10)
-
-# def add_return(a, b):
-#     return a + b
-
-# result = add_return(a=5, b=10)
-
-# result + 20
+def calculate_area(width, height):
+    area = width * height
+    area = area * 1.05
+    return area
+# Store the returned value
+room_area = calculate_area(10, 12)
+print(f"Room size: {room_area} sq. ft.") # Room size is 120 sqft
