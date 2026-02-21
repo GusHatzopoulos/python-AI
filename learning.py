c6c09d0187dcc9e74af3738b6941ebdee2d5954c
Konstantinos Gus Hatzopoulos <k.hatzopoulos@protonmail.com>
2026-02-21 16:29:15 +0200

    Learning simple

--- learning.py
+++ learning.py
@@ -1,3 +1,14 @@
-import math
-math.sqrt(16)
+# # Date and time
+# import datetime
+# today = datetime.date.today()
+# print(today)  # 2024-01-15
 
+# Operating system
+import os
+current_dir = os.getcwd()
+print(current_dir)
+
+# # JSON data
+# import json
+# data = {"name": "Alice", "age": 30}
+# json_string = json.dumps(data)
