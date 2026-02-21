19b56284c5dcf4fed4a1200b2a71d3f132888456
Konstantinos Gus Hatzopoulos <k.hatzopoulos@protonmail.com>
2026-02-21 15:47:51 +0200

    learning simple

--- learning.py
+++ learning.py
@@ -1,17 +1,7 @@
-def double(number):
-    return number * 2
+def simple_function():
+    numbers = [1,2,3,4,5]
+    first_number = numbers[0]
+    last_number = numbers[-1]
+    return first_number, last_number
 
-# # Store in variable
-# result = double(5)
-
-# # Use in expressions
-# total = double(5) + double(3)  # 10 + 6 = 16
-
-# Pass to other functions
-# print(double(10))  # 20
-
-# Use in conditions
-if double(3) > 10:
-    print("Big number!")
-else:
-    print("Result is less than 10.")
+first_number, last_number = simple_function()
